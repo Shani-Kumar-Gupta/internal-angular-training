c9e6407eabdd5ac03c7e9777adb3e2ac3c5f736f
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2024-05-19 22:50:08 +0530

    create behavior, reply and async subject component

---
+++ async-observables/src/app/behavior-subject/behavior-subject.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-behavior-subject',
+  standalone: true,
+  imports: [],
+  templateUrl: './behavior-subject.component.html',
+  styleUrl: './behavior-subject.component.css'
+})
+export class BehaviorSubjectComponent {
+
+}
---
+++ async-observables/src/app/reply-subject/reply-subject.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reply-subject',
+  standalone: true,
+  imports: [],
+  templateUrl: './reply-subject.component.html',
+  styleUrl: './reply-subject.component.css'
+})
+export class ReplySubjectComponent {
+
+}
