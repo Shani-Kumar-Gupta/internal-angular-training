db697c4d62d334c7767754af24573a664d74623b
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2024-05-20 00:02:52 +0530

    Replay Subject

--- async-observables/src/app/reply-subject/reply-subject.component.ts
+++ async-observables/src/app/reply-subject/reply-subject.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-reply-subject',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
   templateUrl: './reply-subject.component.html',
   styleUrl: './reply-subject.component.css'
 })
-export class ReplySubjectComponent {
+export class ReplySubjectComponent implements OnInit {
+  constructor() { }
 
+  ngOnInit() {  
+    /* Replay Subject */
+    // const subject = new Subject();
+    // const subject = new ReplaySubject<number>();
+    const subject = new ReplaySubject<number>(2);
+
+    subject.next(100);
+    subject.next(200);
+    subject.next(300);
+
+
+    // subscriber 1
+    subject.subscribe((data) => {
+      console.log("Data 1: ", data);
+    });
+
+    subject.next(500);
+    subject.next(600);
+
+    // subscriber 2
+    subject.subscribe((data) => {
+      console.log("Data 2: ", data);
+    });
+
+    subject.next(700);
+
+    // subscriber 2
+    subject.subscribe((data) => {
+      console.log("Data 3: ", data);
+    });
+  }
 }
