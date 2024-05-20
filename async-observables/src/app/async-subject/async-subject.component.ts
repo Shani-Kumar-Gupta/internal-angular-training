2a019f89b8f8d1f783d7241cc8bb89b0f8018be9
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2024-05-20 12:10:38 +0530

    Async subject with example

--- async-observables/src/app/async-subject/async-subject.component.ts
+++ async-observables/src/app/async-subject/async-subject.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AsyncSubject } from 'rxjs';
 
 @Component({
   selector: 'app-async-subject',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
   templateUrl: './async-subject.component.html',
   styleUrl: './async-subject.component.css'
 })
-export class AsyncSubjectComponent {
+export class AsyncSubjectComponent implements OnInit {
+  constructor() { }
 
+  ngOnInit() {
+    /* Async Subject */
+    const asyncSubject = new AsyncSubject();
+
+    asyncSubject.next(100);
+    asyncSubject.next(200);
+    asyncSubject.next(300);
+    asyncSubject.complete();
+    asyncSubject.next(400);
+
+    // subsriber 1
+    asyncSubject.subscribe((data) => {
+      console.log("Data 1: ", data);
+    });
+
+    asyncSubject.complete();
+    asyncSubject.next(400);
+
+    // subsriber 2
+    asyncSubject.subscribe((data) => {
+      console.log("Data 2: ", data);
+    });
+  }
 }
